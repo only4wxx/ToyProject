2c59bac42073971f2518e22042a7bd712a4f4161
only4wxx <orchidbyw1@gmail.com>
2024-05-12 18:12:41 +0900

    [Feat] DRF 설치

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,8 @@ PROJECT_APPS = [
 ]
 
 THIRD_PARTY_APPS = [
-
+    'corsheaders',
+    'rest_framework',
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
@@ -69,7 +70,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
